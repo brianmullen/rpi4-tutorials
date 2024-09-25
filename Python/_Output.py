a3dbb5a680dee60b5a95256b1a0091018a7cd492
Brian Mullen <programmer.mullen@gmail.com>
2024-09-25 02:01:37 -0600

    Add state and toggle to LED

--- Python/_Output.py
+++ Python/_Output.py
@@ -1,11 +1,29 @@
+from enum import Enum
 from _Core import OutputPin, PinResistor
+
+class LEDState(Enum):
+    OFF = 1
+    ON = 2
 
 class LED(OutputPin):
     def __init__(self, pin, resistor: PinResistor = PinResistor.NONE):
+        self.__state = LEDState.OFF
         super().__init__(pin, resistor=resistor)
+
+    @property
+    def state(self) -> LEDState:
+        return self.__state
 
     def on(self):
         self.value = 1
+        self.__state = LEDState.ON
     
     def off(self):
         self.value = 0
+        self.__state = LEDState.OFF
+    
+    def toggle(self):
+        if self.__state == LEDState.ON:
+            self.off()
+        else:
+            self.on()
