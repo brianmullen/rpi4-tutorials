f6476a3fa3836f41357fa6ae6a0cb8b222b81e52
Brian Mullen <programmer.mullen@gmail.com>
2024-09-25 16:02:49 -0600

    Add an RGBLED

--- Python/_Output.py
+++ Python/_Output.py
@@ -65,3 +65,59 @@ class PWMLED(LED):
     def close(self):
         self.__pwm.stop()
         super().close()
+
+class RGBLED(object):
+    def __init__(self, red_pin: int, green_pin: int, blue_pin: int, resistor: PinResistor = PinResistor.NONE):
+        self.__red = LED(red_pin, resistor=resistor)
+        self.__green = LED(green_pin, resistor=resistor)
+        self.__blue = LED(blue_pin, resistor=resistor)
+        super().__init__()
+    
+    def __del__(self):
+        if self.__red is not None:
+            self.close()
+
+    @property
+    def red(self) -> LED:
+        return self.__red
+    
+    @property
+    def green(self) -> LED:
+        return self.__green
+    
+    @property
+    def blue(self) -> LED:
+        return self.__blue
+
+    @property
+    def is_closed(self) -> bool:
+        return self.__red is None
+
+    def on(self):
+        if not self.is_closed:
+            self.__red.on()
+            self.__green.on()
+            self.__blue.on()
+    
+    def off(self):
+        if not self.is_closed:
+            self.__red.off()
+            self.__green.off()
+            self.__blue.off()
+    
+    def toggle(self):
+        if not self.is_closed:
+            self.__red.toggle()
+            self.__green.toggle()
+            self.__blue.toggle()
+
+    def close(self):
+        if self.__red is not None:
+            self.__red.close()
+            self.__red = None
+        if self.__green is not None:
+            self.__green.close()
+            self.__green = None
+        if self.__blue is not None:
+            self.__blue.close()
+            self.__blue = None
