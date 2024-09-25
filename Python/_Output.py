d815c3cfb0b28e8f13aeee09ffa7f1f5e7bff085
Brian Mullen <programmer.mullen@gmail.com>
2024-09-25 03:33:05 -0600

    Add a PWMLED

--- Python/_Output.py
+++ Python/_Output.py
@@ -1,3 +1,4 @@
+import RPi.GPIO as GPIO
 from enum import Enum
 from _Core import OutputPin, PinResistor
 
@@ -27,3 +28,40 @@ class LED(OutputPin):
             self.off()
         else:
             self.on()
+
+class PWMLED(LED):
+    def __init__(self, pin, frequency: float = 100, resistor: PinResistor = PinResistor.NONE):
+        self.__frequency = frequency
+        self.__pwm = None
+        self.__duty_cycle = 0.0
+        super().__init__(pin, resistor=resistor)
+        self.__pwm = GPIO.PWM(pin, frequency)
+    
+    @property
+    def frequency(self) -> float:
+        return self.__frequency
+    
+    @frequency.setter
+    def frequency(self, newValue: float):
+        if self.__frequency != newValue:
+            self.__frequency = newValue
+            self.__pwm.ChangeFrequence(newValue)
+    
+    @property
+    def duty_cycle(self) -> float:
+        return self.__duty_cycle
+    
+    @duty_cycle.setter
+    def duty_cycle(self, newValue: float):
+        if self.__duty_cycle != newValue:
+            if self.__duty_cycle == 0 and newValue != 0:
+                self.__pwm.start(newValue)
+            elif self.__duty_cycle !=0 and newValue == 0:
+                self.__pwm.stop()
+            else:
+                self.__pwm.ChangeDutyCycle(newValue)
+            self.__duty_cycle = newValue
+    
+    def close(self):
+        self.__pwm.stop()
+        super().close()
