8fdf599725b137079442fec5116e99e38214bc0d
Brian Mullen <programmer.mullen@gmail.com>
2024-09-25 02:02:04 -0600

    Add a toggle button

--- Python/_Input.py
+++ Python/_Input.py
@@ -11,10 +11,10 @@ class Button(InputPin):
         self.__state = ButtonState.UP
         self.__state_change = None
         if resistor == PinResistor.PULL_UP or resistor == PinResistor.PULL_UP_EXTERNAL:
-            self.__idle_value = 1
+            self._idle_value = 1
         else:
-            self.__idle_value = 0
-        self.__lastValue = self.__idle_value
+            self._idle_value = 0
+        self._lastValue = self._idle_value
         super().__init__(pin, resistor=resistor)
 
     @property
@@ -30,9 +30,27 @@ class Button(InputPin):
         self.__state_change = newValue
 
     def processEvents(self):
-        newValue = self.value
-        if newValue != self.__lastValue:
-            self.__lastValue = newValue
-            self.__state = ButtonState.UP if newValue == self.__idle_value else ButtonState.DOWN
+        self._set_last_value(self.value)
+    
+    def _set_last_value(self, newValue):
+        if self._lastValue != newValue:
+            self._lastValue = newValue
+            newState = ButtonState.UP if newValue == self._idle_value else ButtonState.DOWN
+            self._set_state(newState)
+
+    def _set_state(self, newValue):
+        if self.__state != newValue:
+            self.__state = newValue
             if self.__state_change is not None:
                 self.__state_change(self.__state)
+
+class ToggleButton(Button):
+    def __init__(self, pin, resistor: PinResistor = PinResistor.PULL_UP):
+        super().__init__(pin, resistor=resistor)
+    
+    def _set_last_value(self, newValue):
+        if self._lastValue != newValue:
+            self._lastValue = newValue
+            if newValue == self._idle_value:
+                newState = ButtonState.UP if self.state == ButtonState.DOWN else ButtonState.DOWN
+                self._set_state(newState)
