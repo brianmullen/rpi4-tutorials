568c34655e4b0848968c4eb39cd7b8a2ace083ae
Brian Mullen <programmer.mullen@gmail.com>
2024-10-10 11:18:37 -0600

    Add lesson 61

---
+++ Python/lesson61_camera_track_face_eyes.py
@@ -0,0 +1,58 @@
+import cv2
+from picamera2 import Picamera2
+from time import time
+
+fps = 58.92
+frameSize=(640,480)
+camera=Picamera2()
+# print(camera.sensor_modes)
+camera.preview_configuration.main.size=frameSize
+camera.preview_configuration.main.format="RGB888"
+camera.preview_configuration.controls.FrameRate=fps     # Request 60 fps
+camera.preview_configuration.align()                    # Picks closest standard size to specified size
+camera.configure("preview")
+camera.start()
+
+overlayPosition = (30, 60)
+overlayFont = cv2.FONT_HERSHEY_SIMPLEX
+overlayFontScale = 1.5
+overlayFontColor = (0, 0, 255)                          # opencv uses BGR colorspace
+overlayFontThickness = 2
+
+faceCascade = cv2.CascadeClassifier('/home/brian/code/rpi4-tutorials/Python/haar/haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier('/home/brian/code/rpi4-tutorials/Python/haar/haarcascade_eye.xml')
+
+def updateFps(newValue: float, useLowPassFilter: bool = True):
+    global fps
+
+    if useLowPassFilter:
+        fps = 0.9 * fps + 0.1 * newValue
+    else:
+        fps = newValue
+
+while True:
+    frameStart = time()
+
+    frame = camera.capture_array()
+    frame = cv2.flip(frame, -1)
+    frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    faces = faceCascade.detectMultiScale(frameGray, 1.1, 5)
+    for face in faces:
+        x, y, w, h = face
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
+        faceGray = frameGray[y: y + h, x: x + w]
+        faceColor = frame[y: y + h, x: x + w]
+        eyes = eyeCascade.detectMultiScale(faceGray, 1.1, 5)
+        for eye in eyes:
+            x, y, w, h = eye
+            cv2.rectangle(faceColor, (x, y), (x + w, y + h), (0, 0, 255), 3)
+
+    cv2.putText(frame, str(int(fps)) + " FPS", overlayPosition, overlayFont, overlayFontScale, overlayFontColor, overlayFontThickness)
+
+    cv2.imshow("Camera", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+    frameElapsed = time() - frameStart
+    updateFps(1 / frameElapsed, useLowPassFilter=True)
+cv2.destroyAllWindows()
